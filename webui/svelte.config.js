1ec964d27e69a080003922979ea486d1b98ed907
Zach Riel <zmriel@gmail.com>
2026-04-09 10:43:28 -0400

    chore(deps): update vite and svelte to fix build issue

--- webui/svelte.config.js
+++ webui/svelte.config.js
@@ -5,9 +5,9 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
   // Consult https://svelte.dev/docs/kit/integrations
   // for more information about preprocessors
-  preprocess: vitePreprocess(),
+  preprocess: vitePreprocess({ script: true }),
   kit: {
-    adapter: adapter({ fallback: 'index.html' }),
+    adapter: adapter({ fallback: '200.html' }),
   },
 };
 
